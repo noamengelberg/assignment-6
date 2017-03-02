362efe9e7614bb9234b34a6dbefa819d187d4b3f
noam.engelberg <noam.engelberg@gmail.com>
2017-03-01 20:56:43 -0500

    All slides included

--- js/main.js
+++ js/main.js
@@ -5,12 +5,12 @@
 $(document).ready(function() {
 //Code that executes when the document is ready
 
-var pageNumberCookie = parseInt(Cookies.get('pageNumber'));
+var pageNumberCookie = parseInt(Cookies.get('pageNumber')) || 0;
 console.log(isNaN(pageNumberCookie));
 if (isNaN(pageNumberCookie)) {
   console.log('Empty Cookie');
   Cookies.set('pageNumber', 0);
-  pageNumberCookie = parseInt(Cookies.get('pageNumber')) || 0;
+
 }
 
  var chapters = [];
@@ -21,10 +21,7 @@ if (isNaN(pageNumberCookie)) {
 
     $(".next-button").on('click',
     function(){
-      //chapters[pageNumberCookie].removeClass("active");
-      //chapters[pageNumberCookie].addClass("hidden");
-      //chapters[newPageNumber].removeClass("hidden");
-      //chapters[newPageNumber].addClass("active");
+
       console.log(chapters)
       $(".active").fadeOut(1000);
       var elem = chapters.eq(pageNumberCookie);
@@ -43,13 +40,11 @@ if (isNaN(pageNumberCookie)) {
 
       });
 
-      var newPageNumber = pageNumberCookie + 1;
-      Cookies.set('pageNumber', newPageNumber);
+       pageNumberCookie = pageNumberCookie + 1;
+      console.log(pageNumberCookie)
+      Cookies.set('pageNumber', pageNumberCookie);
 
 
-//Fade out over 1 second
-
-//Fade in over 4 seconds
     }
   )
 });
